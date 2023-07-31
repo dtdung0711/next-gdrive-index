b2d0f7aa5461539e508dd375aeefbaad4ffa92ff
vitocorleone <111355843+dtdung0711@users.noreply.github.com>
2023-07-31 08:31:01 +0700

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // Client ID are safe to expose
   client_id:
-    "126409166174-l2unckghm8d5m1gp3deu0uaps5son64f.apps.googleusercontent.com",
+    "746239575955-c4d2o1ahg4ts6ahm3a5lh5lp9g8m15h4.apps.googleusercontent.com",
   // Client secret and refresh token are encrypted
   // Using encryption key
   // https://drive.mbaharip.com/gudora-setup
   client_secret:
-    "54f0505d11a8fe04d22ec642cdde4728:a6f911c70b0305a51160a2133a9bd2ada3c120567857002311f17bf29cc69e07d7f1a9ca20fd119d684191aee6e3866a",
+    "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",
   refresh_token:
-    "209b4d2ff44bf877ce0636ddc81cdc9a:4a40e172ba5d6c1ee8daf50389841bd2c9d1153163196da0dab63c07b6f305bedafaa209a9e6adbb8d2377c74ba98cf818b6e73b8f57098686ed0b89bec93b0186047ccaab8804310a2ab4e46069d1cac8c322f0f1e206808ac62c18639610be07559eebad904905ba185cae8ba9fbe9",
+    "1//0ezoztzL7obWECgYIARAAGA4SNwF-L9IrC5oObU8u-l6mLcfiNcUS5ryEZPKKZUA3UCNbm5xkHpYXt4g-fx-iQq9HVX92eaQM4EA",
 
   files: {
     // How many files to show per page
